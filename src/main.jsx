2cb3a95f9f53bfee6185589a5ff017c57100fc93
vaishnavic10 <vaishnavichopade316@gmail.com>
2025-02-23 05:25:12 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Import global styles if needed
+import "./index.css"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
